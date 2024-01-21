8f5b14bc40662e3f95a4757db55017fb63bbfcde
itsabhinavm <itsabhinavm05@gmail.com>
2024-01-21 15:06:13 +0530

    updated Create Note UI

--- lib/pages/editPage.dart
+++ lib/pages/editPage.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 //import 'package:flutter_svg/svg.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -11,6 +13,8 @@ class editPage extends StatefulWidget {
 }
 
 class _editPageState extends State<editPage> {
+  int _selectedIndex = 0;
+
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _noteController = TextEditingController();
   List<Map<String, dynamic>> _items = [];
@@ -20,6 +24,12 @@ class _editPageState extends State<editPage> {
         nameController: _nameController.text,
         noteController: _noteController.text)
   ];
+
+  void _navigationBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   final _noteBox = Hive.box("Note_box");
   void initState() {
@@ -48,92 +58,162 @@ class _editPageState extends State<editPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: appBar(),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              _createNote(
-                  {"name": _nameController.text, "note": _noteController.text});
-              Navigator.pop(context);
-            },
-            child: Icon(Icons.add_comment)),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          color: Colors.transparent,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 40),
-                  child: Center(
-                    child: Text(
-                      'What are you thinking about?',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w800),
-                    ),
+      backgroundColor: Colors.transparent,
+      appBar: appBar(),
+      /*floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _createNote(
+                {"name": _nameController.text, "note": _noteController.text});
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.add_comment))*/
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 40),
+                child: Center(
+                  child: Text(
+                    'What are you thinking about?',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w800),
                   ),
                 ),
-                Container(
-                  /*decoration: BoxDecoration(
+              ),
+              Container(
+                /*decoration: BoxDecoration(
                       image: DecorationImage(
                           image: AssetImage("assets/bg1.jpg"),
                           fit: BoxFit.cover)),*/
-                  padding: EdgeInsets.only(top: 15, left: 50, right: 50),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      TextFormField(
-                        style:
-                            TextStyle(color: Colors.deepPurple, fontSize: 25),
-                        controller: _nameController,
-                        decoration: InputDecoration(
-                            hintText: "Enter name",
-                            labelText: "Name",
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 75,
-                                ),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0),
-                                ))),
+                padding: EdgeInsets.only(top: 15, left: 50, right: 50),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(color: Colors.deepPurple, fontSize: 25),
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                          hintText: "Enter name",
+                          labelText: "Name",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.white,
+                                width: 75,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    TextFormField(
+                      style: TextStyle(color: Colors.deepPurple, fontSize: 25),
+                      controller: _noteController,
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                          hintText: "Enter note",
+                          labelText: "Note",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.green,
+                                width: 20,
+                              ),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10.0),
+                              ))),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 96,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: SizedBox(
+                        height: 95,
+                        width: 200,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.red),
+                          child: Text(
+                            "   Exit   ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 28,
+                                color: Colors.white),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
                       ),
-                      SizedBox(
-                        height: 50,
+                    )),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                        child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: SizedBox(
+                        height: 95,
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green),
+                          child: Text(
+                            "Create",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 28,
+                                color: Colors.white),
+                          ),
+                          onPressed: () {
+                            _createNote({
+                              "name": _nameController.text,
+                              "note": _noteController.text
+                            });
+                            Navigator.pop(context);
+                          },
+                        ),
                       ),
-                      TextFormField(
-                        style:
-                            TextStyle(color: Colors.deepPurple, fontSize: 25),
-                        controller: _noteController,
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 5,
-                        decoration: InputDecoration(
-                            hintText: "Enter note",
-                            labelText: "Note",
-                            border: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Colors.green,
-                                  width: 20,
-                                ),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10.0),
-                                ))),
-                      ),
-                      SizedBox(
-                        height: 50,
-                      ),
-                      // place an image here
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ))
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+      /*bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: _selectedIndex,
+          onTap: _navigationBottomBar,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(icon: Icon(Icons.save_as), label: "Save")
+          ]),*/
+    );
   }
 
   AppBar appBar() {
